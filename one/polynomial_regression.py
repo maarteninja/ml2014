523d8be906c3bfac276931d2828a0c7b20fe153d
Elise <koster.elise@gmail.com>
2014-09-03 12:45:41 +0200

    Can now find error in 1.2

--- one/polynomial_regression.py
+++ one/polynomial_regression.py
@@ -31,10 +31,17 @@ def fit_polynomial(x, t, M):
 	'''
 	bias = np.linspace(0, M-1, M).reshape(1,M)
 	x = x.reshape(1, len(x))
+	t = t.reshape(1, len(t))
 	design_matrix = np.power(x.T, bias)
+	w = np.ones(M).reshape(1, M)
+	
+	error = error_function(w, design_matrix, t)
 
-def error_function(w, matrix):
-	pass
+def error_function(w, matrix, t):
+	matrix_w = np.dot(matrix, w.T)
+	matrix_t = matrix_w - t.T
+	error = 0.5 * np.dot(matrix_t.T, matrix_t)
+	return error
 
 vectors = gen_sinusoidal(10)
 fit_polynomial(vectors[0], vectors[1], 5)
