bb9ee47359b33d13889698fd4381bf66ffdb802a
Maarten Inja <maarten.inja@gmail.com>
2014-09-03 17:20:29 +0200

    plot prettier

--- one/lab1.py
+++ one/lab1.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+
 def gen_sinusoidal(n):
 	'''
 	Generates toy data like in MLPR book
@@ -26,67 +27,72 @@ def gen_sinusoidal(n):
 def fit_polynomial(x, t, m):
 	'''
 	Finds maximum-likelihood solution of 
-	unregularized M-th order fit_polynomial
-	for dataset x using t as the target vector.
+	unregularized M-th order fit_polynomial for dataset x using t as the target vector.
 	Returns w -> maximum-likelihood parameter
 	estimates
 
+	x, t = gen_sinusoidal(10)
 	w = fit_polynomial(x, t, 3)
 	'''
+
+	if m < 0:
+		raise ValueError('m can not be negative')
+
+	# plus one for the non-optional first element of the bias vector ^0
+	m += 1
+	# create array of exponents [0, 1, ..., m-1]
 	phi = np.array(range(m))
+	# reserve space for NxM design matrix
 	Phi = np.zeros((np.size(x), m))
-	for i, x_elem in enumerate(x):
+
+	for n, x_elem in enumerate(x):
+		# create array filled with m copies of the nth datapoint
 		x_ar = np.array([x_elem] * m)
-		Phi[i] = x_ar ** phi
+		# multiply with the bias vector
+		Phi[n] = x_ar ** phi
 	Phi = np.matrix(Phi)
+
+	# solve for w
 	return Phi.T.dot(Phi).I.dot(Phi.T).dot(t)
 
 def one_point_three_plot():
+	""" generates 9 sinusoidal points with some noise. Fits 4 models polynomial
+	using least squares solution for m = 0, 1, 3, 9. Pretty plots them all.
+
+	FIXME: for m=9 the plot seems off
+	"""
+
 	n = 9
 	x, y = gen_sinusoidal(n)
 
+	# calculate true f
 	x_points = np.linspace(0, 2*math.pi, 1000)
 	t = np.array([math.sin(i) for i in x_points])
 
-	fig, ax = plt.subplots()
+	for f, m in enumerate([0, 1, 3, 9]):
 
-	for f, m in enumerate([1, 1, 3, 9]):
-		print m
+		# fit for current m
 		w = np.array(fit_polynomial(x, y, m))
-		#for i in range(np.size(w, 1)):
-		#	w.itemset(i, w.item(i) ** i)
-		g = np.array([np.sum(w * x_point ** np.array(range(m))) for x_point in x_points])
 
-		ax = fig.add_subplot(2, 2, f, label='m=%d'%m)
-		ax.plot(x_points, t)
-		ax.plot(x, y, 'o')
-		ax.plot(x_points, g)
+		# for each x: sum for all m:  w[m]*x**m
+		g = [np.sum(w.item(p) * (x_point ** p) for p in range(m+1)) for x_point in x_points]
 
+		# make pretty plot
+		plt.subplot(2, 2, f+1)
+		plt.xlabel('x')
+		plt.ylabel('t')
+		plt.xlim(0, 2*math.pi)
+		plt.ylim(-1.5, 1.5)
+		plt.title('m=%d'%m)
+		plt.tight_layout()
+		plt.plot(x_points, t)
+		plt.plot(x_points, g)
+		plt.plot(x, y, 'o')
+
+	# show actual plot
 	plt.show()
+
 
 one_point_three_plot()
 
 
-#def one_point_three():\n",                                                
-#    \"\"\"Plot for 1.3\"\"\"\n",                                          
-#    x, t = gen_sinusoidal(10)\n",                                         
-#    fig, ax = plt.subplots()\n",                                          
-#    plt.plot(x, t, '.')\n",                                               
-#    plt.ylim((-2, 2))\n",                                                 
-#    plt.xlim((0, 2*np.pi))\n",                                            
-#    for m in [0, 1, 3, 9]:\n",                                            
-#        w = fit_polynomial(x, t, m)\n",                                   
-#        xs = np.arange(0, 2*np.pi, 0.01);\n",                             
-#        ys = []\n",                                                       
-#        for xi in xs:\n",                                                 
-#            y = 0.0\n",                                                   
-#            for i in range(m+1):\n",                                      
-#                y += w.item(i) * (xi ** i)\n",                            
-#            ys.append(y)\n",                                              
-#        plt.plot(xs, ys, label=\"m = \" + str(m))\n",                     
-#    xs = np.arange(0, 2*np.pi, 0.01)\n",                                  
-#    ys = np.sin(xs)\n",                                                   
-#    plt.plot(xs, ys, 'k', label=\"sinusoidal mean\")\n",                  
-#    ax.set_title(\"Fittining Polynomials\")\n",                           
-#    legend = ax.legend(loc='lower left', shadow=True)\n",                 
-#    plt.show()\n",                                      
