3285fc7a6c70f9673324207cbec90fb76c57613e
Bastiaan van der Weij <bjvanderweij@gmail.com>
2014-09-10 12:30:14 +0200

    Refactor question 1.3 and fix plot

--- one/lab1.py
+++ one/lab1.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 def gen_sinusoidal(n):
 	'''
+        QUESTION 1.1
+
 	Generates toy data like in MLPR book
 	Returns N-dimensional vectors x and t, where 
 	x contains evenly spaced values from 0 to 2pi
@@ -26,6 +28,8 @@ def gen_sinusoidal(n):
 
 def fit_polynomial(x, t, m):
 	'''
+        QUESTION 1.2
+
 	Finds maximum-likelihood solution of 
 	unregularized M-th order fit_polynomial for dataset x using t as the target vector.
 	Returns w -> maximum-likelihood parameter
@@ -55,44 +59,49 @@ def fit_polynomial(x, t, m):
 	# solve for w
 	return Phi.T.dot(Phi).I.dot(Phi.T).dot(t)
 
-def one_point_three_plot():
+def plot_sine(linspace):
+    t = np.array([math.sin(i) for i in linspace])
+    plt.plot(linspace, t)
+
+def plot_polynomial(linspace, w):
 	""" generates 9 sinusoidal points with some noise. Fits 4 models polynomial
 	using least squares solution for m = 0, 1, 3, 9. Pretty plots them all.
 
 	FIXME: for m=9 the plot seems off
 	"""
+        # for each x: sum for all m:  w[m]*x**m
+        f = [np.sum(w.item(p) * (x_point ** p) for p in range(np.size(w, 1))) for x_point in linspace]
 
-	n = 9
-	x, y = gen_sinusoidal(n)
+        # make pretty plot
+        plt.plot(linspace, f)
 
-	# calculate true f
-	x_points = np.linspace(0, 2*math.pi, 1000)
-	t = np.array([math.sin(i) for i in x_points])
+def run():
 
-	for f, m in enumerate([0, 1, 3, 9]):
+    N = 10
+    orders = [0, 1, 3, 9]
+    res = 1000
 
-		# fit for current m
-		w = np.array(fit_polynomial(x, y, m))
+    x, t = gen_sinusoidal(N)
+    linspace = np.linspace(0, 2*math.pi, res)
 
-		# for each x: sum for all m:  w[m]*x**m
-		g = [np.sum(w.item(p) * (x_point ** p) for p in range(m+1)) for x_point in x_points]
+    for i, m in enumerate(orders):
+        w = fit_polynomial(x, t, m)
 
-		# make pretty plot
-		plt.subplot(2, 2, f+1)
-		plt.xlabel('x')
-		plt.ylabel('t')
-		plt.xlim(0, 2*math.pi)
-		plt.ylim(-1.5, 1.5)
-		plt.title('m=%d'%m)
-		plt.tight_layout()
-		plt.plot(x_points, t)
-		plt.plot(x_points, g)
-		plt.plot(x, y, 'o')
+        plt.subplot(2, 2, i+1)
+        plt.xlabel('x')
+        plt.ylabel('t')
+        plt.xlim(0, 2*math.pi)
+        plt.ylim(-1.5, 1.5)
+        plt.title('m=%d'%m)
+        plt.tight_layout()
 
-	# show actual plot
-	plt.show()
+        plt.plot(x, t, 'o')
+        
+        plot_sine(linspace)
+        plot_polynomial(linspace, w)
 
-
-one_point_three_plot()
+    plt.show()
 
 
+if __name__ == '__main__':
+    run()
